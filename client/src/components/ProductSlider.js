fe182fd4786ba5e07e7240778cf40aa551b1f4ff
zozo404 <enzocuny66@gmail.com>
2023-10-03 15:49:55 +0200

    🚀 | hero section

--- client/src/components/ProductSlider.js
+++ client/src/components/ProductSlider.js
@@ -4,6 +4,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 // import swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 import '../slider.css';
 // import required modules
 import { Pagination, Navigation } from 'swiper';
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,20 +1,18 @@
 import React from 'react';
-// useFeact hook
-import useFetch from '../hooks/useFetch';
+
 // import compo
-import ProductSlider from '../components/ProductSlider';
+import LatestProducts from '../components/LatestProducts';
+import Hero from '../components/Hero';
 
 const Home = () => {
-  // get new prodcuts
-  const { data } = useFetch('/products?populate=*&filters[isNew]=true');
-  console.log(data);
+  
 
-  return <div className='mb-16'>
-    <div className='container mx-auto'>
-      <h2 className='h2 mb-6 text-center xl:text-left'>Latest Products</h2>
-    </div>
-    <ProductSlider data={data} />
-  </div>;
+  return (
+    <section>
+      <Hero />
+      <LatestProducts />
+    </section>
+  )
 };
 
 export default Home;
