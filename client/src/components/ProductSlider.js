9479044c91855a78baefeeab7c9971d6c2fc83a4
zozo404 <enzocuny66@gmail.com>
2023-10-03 11:32:31 +0200

    💄 | style slider

--- client/src/components/ProductSlider.js
+++ client/src/components/ProductSlider.js
@@ -1,25 +1,49 @@
 import React from 'react';
 // import swiper react compo
-import { Swiper, SwiperClass, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from 'swiper/react';
 // import swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
+import '../slider.css';
 // import required modules
 import { Pagination, Navigation } from 'swiper';
 // compo
 import Product from '../components/Product';
 
 const ProductSlider = ({ data }) => {
-  return <Swiper>
-    <>
-      {data?.map(product => {
-        return <SwiperSlide key={product.id}>
-          <Product product={product} />
-        </SwiperSlide>
-      })}
-    </>
-
-  </Swiper>;
+  return (
+    <Swiper modules={[Pagination, Navigation]} loop={false} navigation={true} breakpoints={{
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 30
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30
+      },
+      1024: {
+        slidesPerView: 2,
+        spaceBetween: 30
+      },
+      1440: {
+        slidesPerView: 5,
+        spaceBetween: 30
+      }
+      }}
+      pagination={{
+        clickable: true,
+      }}
+      className='productSlider mx-auto max-w-[360px] md:max-w-lg xl:max-w-[1410px]'
+    >
+      <>
+        {data?.map(product => {
+          return <SwiperSlide key={product.id}>
+            <Product product={product} />
+          </SwiperSlide>
+        })}
+      </>
+    </Swiper>
+  );
 };
 
 export default ProductSlider;
