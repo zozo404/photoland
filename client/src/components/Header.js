657ff96b01a76478340205d4a08ce9710af9710f
zozo404 <enzocuny66@gmail.com>
2023-10-04 10:40:36 +0200

    ✨  | cart

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,64 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+// images
+import Logo from '../img/logo.png';
+// icons
+import { SlBag } from 'react-icons/sl';
+import { FiMenu } from 'react-icons/fi';
+// link
+import { Link } from 'react-router-dom'
+// compo
+import SearchForm from '../components/SearchForm';
+import CategoryNavMobile from '../components/CategoryNavMobile';
+import Cart from '../components/Cart';
+// cart context
+import { CartContext } from '../context/CartContext';
 
 const Header = () => {
-  return <div>Header</div>;
+  const { IsOpen, setIsOpen } = useContext(CartContext);
+  const [catNavMobile, setCatnavMobile] = useState(false);
+  return <header className='bg-primary py-6 fixed w-full top-0 z-40 lg:relative xl:mb-[30px]'>
+    <div className='container mx-auto'>
+      <div className=' flex flex-row gap-4 lg:items-center justify-between mb-4 lg:mb-0'>
+        {/* menu */}
+        <div onClick={() => setCatnavMobile(true)} className='text-3xl xl:hidden cursor-pointer'>
+          <FiMenu />
+        </div>
+        {/* category nav mobile */}
+        <div className={`${catNavMobile ? 'left-0' : '-left-full'} fixed top-0 bottom-0 z-30 w-full h-screen transition-all duration-200`}>
+          <CategoryNavMobile setCatnavMobile={setCatnavMobile} />
+        </div>
+        {/* logo */}
+        <Link to={'/'}>
+          <img src={Logo} alt='logo' />
+        </Link>
+        {/* searchform - show only dedsktop */}
+        <div className='hidden w-full xl:flex xl:max-w-[734px]'>
+          <SearchForm />
+        </div>
+        {/* phone & cart */}
+        <div>
+          {/* phone */}
+          <div>Need help? 0606060606</div>
+          {/* cart icon */}
+          <div onClick={() => setIsOpen(!IsOpen)} className='relative cursor-pointer'>
+            <SlBag className='text-2xl' />
+            {/* amount */}
+            <div>2</div>
+          </div>
+          {/* cart */}
+          <div className={`
+          ${IsOpen ? 'right-0' : ' -right-full '}
+          bg-[#0e0f10] shadow-xl fixed top-0 bottom-0 w-full z-10 md:max-w-[500px] transition-all duration-300 `}>
+            <Cart />
+          </div>
+        </div>
+      </div>
+      {/* searchform - show on mobile only */}
+      <div className='lg:hidden'>
+        <SearchForm />
+      </div>
+    </div>
+  </header>;
 };
 
 export default Header;
