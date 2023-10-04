9501c9ab51b7b2405c1ef3ccc218fdfe08ed0e41
zozo404 <enzocuny66@gmail.com>
2023-10-04 18:54:10 +0200

    ✨ |searchform

--- client/src/components/SearchForm.js
+++ client/src/components/SearchForm.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 // icons
 import { FiSearch } from 'react-icons/fi';
+// useNavigate hook
+import { useNavigate } from 'react-router-dom';
 
 const SearchForm = () => {
-  return <form className='relative'>
-    <input className='input' type='text' placeholder='Search for a product...'/>
+  const navigate = useNavigate();
+  const [searchTerm, setSearchTerm] = useState('');
+  const [isAnimating, setIsAnimating] = useState(false);
+
+  useEffect(()=>{
+    const timeout = setTimeout(()=>{
+      setIsAnimating(false);
+    }, 1000);
+    // clear timeout event
+    return () => clearTimeout(timeout);
+  })
+
+  const handleSearchInput = (e) => {
+    setSearchTerm(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(searchTerm);
+    if (searchTerm.length > 0) {
+      navigate(`/search?query=${searchTerm}`);
+      document.querySelector('input').value = '';
+      setSearchTerm('')
+    } else {
+      // if input is empty animation to true
+     setIsAnimating(true);
+    }
+  }
+  return <form onSubmit={handleSubmit} className={`${isAnimating ? 'animate-shake' : 'animate-none'} w-full relative`}>
+    <input onChange={handleSearchInput} className='input' type='text' placeholder='Search for a product...' />
     <button className='btn btn-accent absolute top-0 right-0 rounded-tl-none rounded-bl-none'>
-      <FiSearch className='text-xl'/>
+      <FiSearch className='text-xl' />
     </button>
   </form>;
 };
